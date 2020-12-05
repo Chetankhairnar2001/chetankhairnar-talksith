9fbf811f080ef24a7e45757398bb033982807705
ChetanKhairnar <chetan.khairnar2001@gmail.com>
2020-12-05 21:02:15 +0530

    minor changes

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ var inputtext=document.querySelector("#inputtxt");
 var translatebtn=document.querySelector("#inputbtn");
 var outputtext=document.querySelector("outputtxt");
 
+
 var urlforwakanda="https://api.funtranslations.com/translate/sith.json";
 function translateurl(text){
-    return urlforwakanda+"?text="+text;
+    return urlforwakanda +"?text="+text;
 }
 
 function clickme(){
     var input=inputtext.value;
 
-   fetch(translateurl(input)).then(resp =>resp.json())
+   fetch(translateurl(input))
+   .then(resp =>resp.json())
    .then(json =>{ var translatetxt=json.contents.translated;
        outputtext.innerText=translatetxt })
 };
