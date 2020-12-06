9f85ef1832c64995ca0fde146db4cde69033d34e
ChetanKhairnar <chetan.khairnar2001@gmail.com>
2020-12-06 11:15:21 +0530

    added id selector to outputtext

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
 var inputtext=document.querySelector("#inputtxt");
 var translatebtn=document.querySelector("#inputbtn");
-var outputtext=document.querySelector("outputtxt");
+var outputtext=document.querySelector("#outputtxt");
 
-var input=inputtext.value;
+var urlforsith="https://api.funtranslations.com/translate/sith.json";
 
-var urlforwakanda="https://api.funtranslations.com/translate/sith.json";
-translatebtn.addEventListener("click",function clickme(){
-    outputtext.innerText=input;
-});
-// function translateurl(text){
-//     return urlforwakanda +"?text="+text;
-// }
-// console.log(outputtext);
-// function clickme(){
-//     var input=inputtext.value;
+function translateurl(text){
+    return urlforsith +"?text="+text;
+}
 
-//    fetch(translateurl(input))
-//    .then(resp =>resp.json())
-//    .then(json =>{
-//        var translatetxt=json.contents.translated;
-//        outputtext.innerText=translatetxt; })
-// };
+function clickme(){
+    var input=inputtext.value;
 
-// translatebtn.addEventListener("click",clickme);
+   fetch(translateurl(input))
+   .then(resp =>resp.json())
+   .then(json =>{
+       var translatetxt=json.contents.translated;
+       outputtext.innerText=translatetxt; })
+};
 
-// translatebtn.addEventListener("click",translate);
-// function translate(){
-//      fetch(urlforwakanda+"?text="+input).then(resp=>resp.json()).then(sith=>console.log(sith));
-// }
+translatebtn.addEventListener("click",clickme);
+
